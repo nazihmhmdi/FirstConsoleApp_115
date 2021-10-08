b3c6941bac894cd25fb04ef469c3db607b2d60f9
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2021-10-08 11:10:57 +0700

    Add project files.

---
+++ FirstConsoleApp_115/Program.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+
+namespace FirstConsoleApp_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Muhammad Nazih Mahmudi");
+            Console.WriteLine("20190140115");
+        }
+    }
+}
